c73f1532ac2f2db8b00efee5e8ceee22dee1af84
matias pizarro <matias@pizarro.net>
2015-07-21 21:50:25 +0200

    Sets pybsd-ezjail's separate codebase

--- docbase/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function, absolute_import
-from systems import Master, DummyMaster, Jail
-
-mpizarro = DummyMaster('mpizarro')
-mpizarro.ezjail_admin('list')
