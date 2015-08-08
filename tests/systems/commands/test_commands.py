8a1240a888a3d86cb05abef10dd5faeffc338ebe
matias pizarro <matias@pizarro.net>
2015-08-08 14:31:31 +0200

    Adds a couple of systems.commands tests

--- tests/systems/commands/test_commands.py
+++ tests/systems/commands/test_commands.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, absolute_import
+import six
 import unittest
 from pybsd.systems import System, SystemError
 from pybsd.systems.commands import BaseCommand, CommandError
@@ -13,15 +14,38 @@ class NoBinaryCommand(BaseCommand):
 
 class BaseCommandTestCase(unittest.TestCase):
 
+    params = {
+        'name': 'system',
+        'hostname': 'system.foo.bar',
+        'ext_if': ('re0', ['8.8.8.8/24']),
+        'int_if': ('eth0', ['192.168.0.0/24'])
+    }
+
+    def setUp(self):
+        self.system = System(**self.params)
+
     def test_no_name_command(self):
         with self.assertRaises(CommandError) as context_manager:
             _bc = NoNameCommand(env='something')
         self.assertEqual(context_manager.exception.message, u'`name` property is missing')
 
-    def test_env_executor(self):
+    def test_env_no_executor(self):
         with self.assertRaises(SystemError) as context_manager:
             _bc = NoBinaryCommand(env='something')
         self.assertEqual(context_manager.exception.message, u'`something` must have a callable Executor method')
+
+    def test_env_executor_not_callable(self):
+        self.system._exec = None
+        with self.assertRaises(SystemError) as context_manager:
+            _bc = NoBinaryCommand(env='something')
+        self.assertEqual(context_manager.exception.message, u'`something` must have a callable Executor method')
+
+    def test_no_binary_command(self):
+        _bc = NoBinaryCommand(env=self.system)
+        with self.assertRaises(NotImplementedError) as context_manager:
+            _bc.invoke()
+        message = context_manager.exception.message if six.PY2 else context_manager.exception.args[0]
+        self.assertEqual(message, u'`command_name` is not implemented on this system')
 
     # def test_no_binary_command(self):
     #     with self.assertRaises(SystemError) as context_manager:
